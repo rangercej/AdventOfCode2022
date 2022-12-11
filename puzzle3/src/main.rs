c53f4ce5d19288b5a7035063f72ee682c30eb75c
Chris J <cej@nightwolf.org.uk>
2022-12-11 23:00:51 +0000

    Some cleanup, removed external crate (wasn't really needed)

--- puzzle3/src/main.rs
+++ puzzle3/src/main.rs
@@ -1,5 +1,4 @@
 use std::io::BufRead;
-use array_tool::vec::Intersect;
 
 fn main() {
     let br = common::get_inputfilereader();
@@ -9,26 +8,26 @@ fn main() {
         let l = line.unwrap();
         let half_ln = l.chars().count() / 2;
 
-        let contain1: Vec<char> = l[..half_ln].chars().collect();
-        let contain2: Vec<char> = l[half_ln..].chars().collect();
+        let contain1 = &l[..half_ln];
+        let contain2 = &l[half_ln..];
 
         // Sanity check that string was properly halved.
         if contain1.len() != contain2.len() {
             panic!("Different lengths!");
         }
 
-        // Note for future self: Vectors don't implement copy, so we need to
-        // explicitly clone so we can dump contain2 into debug output later on.
-        let result = contain1.intersect(contain2.clone());
-        let c = result[0] as u32;
+       let mut c = 0;
+       'outer: for c1 in contain1.chars() {
+            for c2 in contain2.chars() {
+                if c1 == c2 {
+                    c = c1 as u32;
+                    break 'outer;
+                }
+            }
+        };
 
         // Finally! A-Z = 27-52 (from 65-90), a-z = 1-26 (from 97-122)
         let val = if c >= 92 { c - 96 } else { c - 65 + 27 };
-
-        println!("{} = {} + {} => {}, {}, {}", l, 
-            contain1.into_iter().collect::<String>(), 
-            contain2.into_iter().collect::<String>(),
-            result[0], c, val);
 
         total += val;
     }
