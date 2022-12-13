83c7a16a7e74d4b7ef9641b6ed6f49cec3210be9
Chris J <cej@nightwolf.org.uk>
2022-12-13 20:23:13 +0000

    Day 3 part 2 complete

--- puzzle3/src/main.rs
+++ puzzle3/src/main.rs
@@ -1,6 +1,11 @@
 use std::io::BufRead;
 
 fn main() {
+    part1();
+    part2();
+}
+
+fn part1() {
     let br = common::get_inputfilereader();
 
     let mut total = 0;
@@ -33,4 +38,60 @@ fn main() {
     }
 
     println!("Total = {}", total);
+}
+
+fn part2() {
+    let br = common::get_inputfilereader();
+
+    let mut total = 0;
+    let mut group = std::vec::Vec::new();
+    let mut n = 0;
+ 
+    for line in br.lines() {
+        let l = line.unwrap();
+        group.push(l);
+        n += 1;
+
+        if n == 3 {
+            n = 0;
+            let result1 = intersect(&group[0], &group[1]);
+            let result2 = intersect(&result1, &group[2]);
+
+            // Sanity check
+            if result2.len() == 0 {
+                println!("No match for group:");
+                println!("        {}", group[0]);
+                println!("        {}", group[1]);
+                println!("        {}", group[2]);
+            }
+ 
+            let c = result2.chars().nth(0).unwrap() as u32;
+
+            // Finally! A-Z = 27-52 (from 65-90), a-z = 1-26 (from 97-122)
+            let val = if c >= 92 { c - 96 } else { c - 65 + 27 };
+
+            total += val;
+
+            group.remove(0);
+            group.remove(0);
+            group.remove(0);
+        }
+    }
+
+    println!("Total = {}", total);
+}
+
+fn intersect(s1: &String, s2: &String) -> String
+{
+    let mut result: String = std::string::String::new();
+
+    for c1 in s1.chars() {
+         for c2 in s2.chars() {
+             if c1 == c2 {
+                 result.push(c1);
+             }
+         }
+     };
+
+     result
 }
