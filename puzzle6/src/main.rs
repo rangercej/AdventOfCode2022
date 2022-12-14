0f13e79c8bcd5a4b4335f1f80cd263013dbcae58
Chris J <cej@nightwolf.org.uk>
2022-12-14 19:23:49 +0000

    Refactor, remove dupe code

--- puzzle6/src/main.rs
+++ puzzle6/src/main.rs
@@ -6,15 +6,17 @@ fn main() {
     let mut line = String::with_capacity(500);
     br.read_line(&mut line).unwrap();
 
-    get_header_posn(&line);
-    get_message_posn(&line);
+    let header_pos = get_unique_posn(&line, 4);
+    let message_pos = get_unique_posn(&line, 14);
+
+    println!("Header position = {}", header_pos);
+    println!("Message position = {}", message_pos);
 }
 
-fn get_header_posn(line: &str)
+fn get_unique_posn(line: &str, buflen: usize) -> i32
 {
     let mut buf: Vec<char> = std::vec::Vec::new();
     let mut cnt = 0;
-    let buflen = 4;
 
     for c in line.chars() {
         cnt += 1;
@@ -27,27 +29,7 @@ fn get_header_posn(line: &str)
         }
     }
 
-    println!("Marker starts at {}", cnt);
-}
-
-fn get_message_posn(line: &str)
-{
-    let mut buf: Vec<char> = std::vec::Vec::new();
-    let mut cnt = 0;
-    let buflen = 14;
-
-    for c in line.chars() {
-        cnt += 1;
-        append_buf(&mut buf, c, buflen);
-
-        if buf.len() == buflen {
-            if !contains_dupe(&buf) {
-                break;
-            }
-        }
-    }
-
-    println!("Message starts at {}", cnt);
+    cnt
 }
 
 fn append_buf(buf: &mut Vec<char>, c: char, maxsize: usize)
@@ -61,18 +43,14 @@ fn append_buf(buf: &mut Vec<char>, c: char, maxsize: usize)
 
 fn contains_dupe(buf:&Vec<char>) -> bool
 {
-    let mut old = '*';
-    let mut dup = false;
+    let mut chars = String::new();
 
-    let mut bufcopy = buf.clone();
-    bufcopy.sort();
-    for ch in bufcopy.iter() {
-        if *ch == old {
-            dup = true;
-            break;
+    for ch in buf.iter() {
+        if chars.contains(*ch) {
+            return true;
         }
-        old = *ch;
+        chars.push(*ch);
     }
 
-    dup
+    false
 }
