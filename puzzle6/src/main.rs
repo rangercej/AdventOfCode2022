41e9790f6310b2cf5ea91440c74533806d36d64b
Chris J <cej@nightwolf.org.uk>
2022-12-14 19:14:34 +0000

    Common dupe checker

--- puzzle6/src/main.rs
+++ puzzle6/src/main.rs
@@ -14,17 +14,14 @@ fn get_header_posn(line: &str)
 {
     let mut buf: Vec<char> = std::vec::Vec::new();
     let mut cnt = 0;
+    let buflen = 4;
 
     for c in line.chars() {
         cnt += 1;
-        append_buf(&mut buf, c, 4);
+        append_buf(&mut buf, c, buflen);
 
-        if buf.len() == 4 {
-            if buf[0] == buf[1] || buf[0] == buf[2] || buf[0] == buf[3]
-                        || buf[1] == buf[2] || buf[1] == buf[3]
-                        || buf[2] == buf[3] {
-                    continue;
-            } else {
+        if buf.len() == buflen {
+            if !contains_dupe(&buf) {
                 break;
             }
         }
@@ -37,31 +34,14 @@ fn get_message_posn(line: &str)
 {
     let mut buf: Vec<char> = std::vec::Vec::new();
     let mut cnt = 0;
+    let buflen = 14;
 
     for c in line.chars() {
         cnt += 1;
-        append_buf(&mut buf, c, 14);
+        append_buf(&mut buf, c, buflen);
 
-        if buf.len() == 14 {
-            let mut old = '*';
-            let mut dup = false;
-            
-            // Need a nicer way of doing this other than
-            // cloning the vector on each iteration. Feels
-            // icky. Works, but icky. The "if" chain in
-            // get_header_posn() only really practical for
-            // short lengths.
-            let mut bufcopy = buf.clone();
-            bufcopy.sort();
-            for ch in bufcopy.iter() {
-                if *ch == old {
-                    dup = true;
-                    break;
-                }
-                old = *ch;
-            }
-
-            if !dup {
+        if buf.len() == buflen {
+            if !contains_dupe(&buf) {
                 break;
             }
         }
@@ -77,4 +57,22 @@ fn append_buf(buf: &mut Vec<char>, c: char, maxsize: usize)
     }
 
     buf.push(c);
+}
+
+fn contains_dupe(buf:&Vec<char>) -> bool
+{
+    let mut old = '*';
+    let mut dup = false;
+
+    let mut bufcopy = buf.clone();
+    bufcopy.sort();
+    for ch in bufcopy.iter() {
+        if *ch == old {
+            dup = true;
+            break;
+        }
+        old = *ch;
+    }
+
+    dup
 }
